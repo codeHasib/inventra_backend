7d6f5a9d463da0de4b49b4892e3fa3b7e5db995e
codeHasib <mohammadhasib9827@gmail.com>
2026-07-13 16:31:58 +0600

    fixed api protection issue

--- src/app.ts
+++ src/app.ts
@@ -21,7 +21,7 @@ app.use(
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization", "Cookie", "Accept"],
+    allowedHeaders: ["Content-Type", "Authorization", "Cookie", "Accept", "Cache-Control", "Pragma", "Expires"],
     exposedHeaders: ["set-cookie"],
   }),
 );
